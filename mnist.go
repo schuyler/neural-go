6fe7e123441bfc6a22545d0bbec3e80a0a2cffa7
Schuyler Erle <schuyler@nocat.net>
2011-10-17 14:16:56 -0700

    Train on MNIST digits.

--- mnist.go
+++ mnist.go
@@ -1,5 +1,6 @@
 package main
 import (
+    "neural"
     "encoding/binary"
     "io"
     "os"
@@ -14,15 +15,14 @@ func ReadMNISTLabels (r io.Reader) (labels []byte) {
     return
 }
 
-func ReadMNISTImages (r io.Reader) (images [][]byte) {
+func ReadMNISTImages (r io.Reader) (images [][]byte, width, height int) {
     header := [4]int32{}
     binary.Read(r, binary.BigEndian, &header)
     images = make([][]byte, header[1])
-    imageSize := header[2] * header[3]
+    width, height = int(header[2]), int(header[3])
     for i := 0; i < len(images); i++ {
-        image := make([]byte, imageSize)
-        r.Read(image)
-        images[i] = image
+        images[i] = make([]byte, width * height)
+        r.Read(images[i])
     }
     return
 }
@@ -48,10 +48,41 @@ func OpenFile (path string) *os.File {
 }
 
 func main () {
-    labels := ReadMNISTLabels(OpenFile(os.Args[1]))
-    images := ReadMNISTImages(OpenFile(os.Args[2]))
-    fmt.Println("Labels =", len(labels), "Images =", len(images), "Image Size =", len(images[0]))
-    for _, image := range images {
-        fmt.Println(ImageString(image, 28, 28))
+    const numLabels = 10
+    const epsilon = 0.001
+
+    labelData := ReadMNISTLabels(OpenFile(os.Args[1]))
+    imageData, width, height := ReadMNISTImages(OpenFile(os.Args[2]))
+
+    labels := make([][]float64, len(labelData))
+    for i, value := range labelData {
+        labels[i] = make([]float64, numLabels)
+        labels[i][value] = 1.0
+    }
+
+    images := make([][]float64, len(imageData))
+    for i, vector := range imageData { 
+        images[i] = make([]float64, len(vector))
+        for j := 0; j < len(images[i]); j++ {
+            images[i][j] = float64(vector[j])/255.0
+        }
+    }
+
+    neural.SeedRandom()
+    net := neural.NewNetwork(width * height, 100, numLabels)
+    epoch, best := 0, epsilon;
+    for ; best >= epsilon; epoch++ {
+        best = 0.0
+        for i, expected := range labels {
+            input := images[i]
+            result := net.Activate(input)
+            net.Train(input, expected, 0.75, 0.25)
+            err := neural.MeanSquaredError(result, expected)
+            if err > best { best = err }
+            if i % 1000 == 0 {
+                fmt.Printf("\rEpoch # %d: %d%%", epoch, int(float32(i)/float32(len(labels))*100.0))
+            }
+        }
+        fmt.Println("\rEpoch #", epoch, "@ MSE =", best)
     }
 }
