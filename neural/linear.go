67380567f4735f37b3a1d6cc8a8e9e244cee67bb
Schuyler Erle <schuyler@nocat.net>
2013-10-16 00:50:04 -0700

    Add tests for LinearLayer and make them work.

--- neural/linear.go
+++ neural/linear.go
@@ -10,56 +10,53 @@ type LinearLayer struct {
     layerBase
 }
 
-func MakeLinearLayer(inputs, outputs int, rate float64) (layer *LinearLayer) {
-    layer = new(LinearLayer)
-    layer.weights = matrix.Normals(inputs, outputs)
-    layer.bias = matrix.Normals(1, outputs)
-    layer.rate = rate
-    return
+func MakeLinearLayer(inputs, outputs int) (layer *LinearLayer) {
+    return &LinearLayer{
+        weights: matrix.Normals(inputs, outputs), 
+        bias: matrix.Normals(1, outputs)}
 }
 
-func (layer *LinearLayer) Activate(input matrix.MatrixRO) (output matrix.MatrixRO) {
+func (layer *LinearLayer) Activate(input matrix.MatrixRO) (matrix.MatrixRO, error) {
+    var product, output matrix.Matrix
     var err error
-    var product matrix.Matrix
     layer.input = input
     // f(x) = W * x + b  
     product, err = input.Times(layer.weights)
     if err != nil {
-        panic(err)
+        return nil, err
     }
     output, err = product.Plus(layer.bias)
     if err != nil {
-        panic(err)
+        return nil, err
     }
     layer.output = output
-    return
+    return output, nil
 }
 
-func (layer *LinearLayer) Train(cost matrix.MatrixRO, rate float64) (residual matrix.MatrixRO) {
-    var err error
+func (layer *LinearLayer) Train(cost matrix.MatrixRO, rate float64) (residual matrix.MatrixRO, err error) {
     var weight_gradient matrix.Matrix
     // dC/dx = transpose(W) x dC/d(f(x)) 
-    residual, err = matrix.Transpose(layer.weights).Times(cost)
+    residual, err = layer.weights.Times(cost)
     if err != nil {
-        panic(err)
+        return nil, err
     }
     // dC/dW = dC/d(f(x)) x transpose(x)
-    weight_gradient, err = cost.Times(matrix.Transpose(layer.input))
+    weight_gradient, err = cost.Times(layer.input)
     if err != nil {
-        panic(err)
+        return nil, err
     }
     // scale the gradient by the learning rate and update the weights
     weight_gradient.Scale(rate)
-    err = layer.weights.Add(weight_gradient)
+    err = layer.weights.Add(matrix.Transpose(weight_gradient))
     if err != nil {
-        panic(err)
+        return nil, err
     }
     // dC/db = dC/d(f(x)) ... which is just the cost
     // scale the gradient by the learning rate and update the bias
-    bias_gradient := matrix.Scaled(cost, layer.rate)
+    bias_gradient := matrix.Scaled(cost, rate)
     err = layer.bias.Add(bias_gradient)
     if err != nil {
-        panic(err)
+        return nil, err
     }
-    return
+    return residual, nil
 }
--- neural/linear_test.go
+++ neural/linear_test.go
@@ -1,15 +1,50 @@
 package neural
 
 import (
+    . "github.com/smartystreets/goconvey/convey"
+    "github.com/skelterjohn/go.matrix"
     "testing"
 )
 
 func TestMakeLinearLayer (t *testing.T) {
-    MakeLinearLayer(3, 4)
+    Convey("When creating a new LinearLayer", t, func() {
+        Convey("the constructor should succeed", func() {
+            So(func() { MakeLinearLayer(3, 1) }, ShouldNotPanic)
+        })
+    })
 }
 
 func TestActivate (t *testing.T) {
-    layer := MakeLinearLayer(3, 1, 0.1)
-    input := matrix.MakeDenseMatrix(&[]float64{0.0, 0.0, 0.0})
+    Convey("When activating a LinearLayer with a unit input", t, func() {
+        layer := &LinearLayer{weights: matrix.Ones(3, 1), bias: matrix.Zeros(1, 1)}
+        input := matrix.Ones(1, 3)
+        Convey("the output should be the product of the input and weights plus the bias", func() {
+            output, err := layer.Activate(input)
+            So(err, ShouldBeNil)
+            rows, cols := output.GetSize()
+            So(rows, ShouldEqual, 1)
+            So(cols, ShouldEqual, 1)
+            So(output, ShouldResemble, matrix.Numbers(1, 1, 3))
+        })
+    })
+}
 
+func TestTrain (t *testing.T) {
+    Convey("When training a LinearLayer", t, func() {
+        layer := MakeLinearLayer(3, 1)
+        Convey("the MSE should improve at every epoch", func() {
+            input := matrix.Ones(1, 3) // {1, 1, 1}
+            expected := matrix.Numbers(1, 1, 3.0) // {3}
+            epochs := 20
+            for delta := 1.0; epochs > 0 && delta > 0.01; epochs-- {
+                output, _ := layer.Activate(input)
+                cost, _ := expected.Minus(output)
+                _, err := layer.Train(cost, 0.1)
+                So(err, ShouldBeNil)
+                So(delta, ShouldBeGreaterThan, cost.Get(0,0))
+                delta = cost.Get(0, 0)
+            }
+            So(epochs, ShouldBeGreaterThan, 0)
+        })
+    })
 }
